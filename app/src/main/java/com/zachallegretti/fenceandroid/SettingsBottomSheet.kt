e32686bdf048e727b1c631b7497466e199784d3f
zachallegretti <allegretti813@gmail.com>
2023-08-14 23:55:32 -0700

    Add UI for remaining items in Settings bottomsheet

--- app/src/main/java/com/zachallegretti/fenceandroid/SettingsBottomSheet.kt
+++ app/src/main/java/com/zachallegretti/fenceandroid/SettingsBottomSheet.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
-import android.widget.ListAdapter
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.textfield.TextInputLayout
 
@@ -20,10 +19,19 @@ class SettingsBottomSheet : BottomSheetDialogFragment() {
         val view = inflater.inflate(R.layout.settings_bottom_sheet, container, false)
 
         val boutType = view.findViewById<TextInputLayout>(R.id.bout_type_menu)
-        val adapter = (ArrayAdapter<String>(requireContext(), R.layout.bout_type_list_item, BOUT_TYPE_LIST))
+        val adapter = (ArrayAdapter<String>(requireContext(), R.layout.dropdown_menu_item, BOUT_TYPE_LIST))
         (boutType.editText as? AutoCompleteTextView)?.setAdapter(adapter)
         (boutType.editText as? AutoCompleteTextView)?.setText(getSelectedBoutType(), false)
 
+        val weaponType = view.findViewById<TextInputLayout>(R.id.weapon_menu)
+        val weaponAdapter = (ArrayAdapter<String>(requireContext(), R.layout.dropdown_menu_item, WEAPON_LIST))
+        (weaponType.editText as? AutoCompleteTextView)?.setAdapter(weaponAdapter)
+        (weaponType.editText as? AutoCompleteTextView)?.setText(getSelectedWeaponType(), false)
+
+        val practiceLength = view.findViewById<TextInputLayout>(R.id.practice_length_menu)
+        val practiceAdapter = (ArrayAdapter<String>(requireContext(), R.layout.dropdown_menu_item, PRACTICE_LIST))
+        (practiceLength.editText as? AutoCompleteTextView)?.setAdapter(practiceAdapter)
+        (practiceLength.editText as? AutoCompleteTextView)?.setText(getSelectedPracticeLength(), false)
 
         return view
     }
@@ -33,9 +41,21 @@ class SettingsBottomSheet : BottomSheetDialogFragment() {
         return "${BOUT_TYPE_LIST.first()}"
     }
 
+    private fun getSelectedWeaponType(): String {
+        //TODO: Add real implementation
+        return WEAPON_LIST.first()
+    }
+
+    private fun getSelectedPracticeLength(): String {
+        //TODO: Add real implementation
+        return PRACTICE_LIST.first()
+    }
+
     companion object {
         const val TAG = "SettingsBottomSheet"
         val BOUT_TYPE_LIST = listOf("Practice", "Pool", "Team", "DE", "DE (VET)", "DE(Y10)")
+        val WEAPON_LIST = listOf("Epee", "Foil", "Saber")
+        val PRACTICE_LIST = listOf("5", "10", "15", "99")
     }
 
 }
